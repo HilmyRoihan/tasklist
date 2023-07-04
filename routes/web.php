ebce3760f6509bc03cce9b6a8427b1f8c215cbce
unknown <hilmyroihan@gmail.com>
2023-07-04 20:34:59 +0700

    first commite

---
+++ routes/web.php
@@ -0,0 +1,113 @@
+<?php
+use App\Http\Requests\TaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks' => Task::latest()->paginate()
+    ]);
+})->name('tasks.index');
+
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
+
+// Rute edit  
+    Route::get('/tasks/{task}/edit', function (Task $task) {
+        return view('edit', [
+            'task' => $task
+        ]);
+    })->name('tasks.edit');
+
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show', [
+        'task' => $task
+    ]);
+})->name('tasks.show');
+
+
+// RUTE ADD
+Route::post('/tasks', function (TaskRequest $request) {
+   
+             //VERSI PANJANG
+    // $data = $request->validated();
+    // $task = new Task;
+    // $task->title = $data['title'];
+    // $task->description = $data['description'];
+    // $task->long_description = $data['long_description'];
+    // $task->save();
+
+   $task= Task::create($request->validated());
+
+    return redirect()->route('tasks.show', ['task' => $task->id ])
+    ->with ('success', 'Task Created Successfully!');
+})->name('tasks.store');
+
+
+// RUTE EDIT
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+            //VERSI PANJANG
+    // $data = $request->validated();
+    // $task->title = $data['title'];
+    // $task->description = $data['description'];
+    // $task->long_description = $data['long_description'];
+    // $task->save();
+
+    $task->update($request->validated());
+
+    return redirect()->route('tasks.show', ['task' => $task->id])
+    ->with('success', 'Task Updated Successfully!');
+})->name('tasks.update');
+
+//RUTE DELETE
+
+Route::delete('/tasks/{task}', function(Task  $task){
+    $task->delete();
+
+    return redirect()->route('tasks.index')
+    ->with('success','Task deleted successfully!');
+}) ->name ('tasks.destroy'); 
+
+
+
+Route::put('tasks/{task}/toggle-complete', function (Task $task){
+    $task->toggleComplete();
+
+    return redirect()->back()->with('success', 'Task Updated Successfully!');
+}) ->name('tasks.toggle-complete');
+
+// Route::get('/xxx', function () {
+//     return 'Hello';
+// })->name('hello');
+
+// Route::get('/hallo', function () {
+//     return redirect()->route('hello');
+// });
+
+// Route::get('/greet/{name}', function ($name) {
+//     return 'Hello ' . $name . '!';
+// });
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
